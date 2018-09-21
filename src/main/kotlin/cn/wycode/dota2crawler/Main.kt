16d244a5bf7aad85348a52ebc027e200b4d09a8a
wangyu <www_wayne_001@163.com>
2018-09-21 17:55:07 +0800

    爬取英雄列表，准备存入H2数据库

--- src/main/kotlin/cn/wycode/dota2crawler/Main.kt
+++ src/main/kotlin/cn/wycode/dota2crawler/Main.kt
@@ -1,21 +1,26 @@
 package cn.wycode.dota2crawler
 
-import java.io.FileInputStream
+import java.sql.DriverManager
+import java.sql.SQLException
 import java.util.*
 
 fun main(args: Array<String>) {
+
+    Class.forName("org.h2.Driver")
     val resourceBundle = ResourceBundle.getBundle("application")
     val url = resourceBundle.getString("datasource.url")
     println(url)
-//    val url = "jdbc:h2:tcp://localhost/H2DB/wycode"
-//    val username = "wayne"
-//    val password = "11900503"
-//    val con = try {
-//        DriverManager.getConnection(url, username, password)
-//    } catch (se: SQLException) {
-//        se.printStackTrace()
-//        return
-//    }
+    val username = resourceBundle.getString("datasource.username")
+    val password = resourceBundle.getString("datasource.password")
+    val con = try {
+        DriverManager.getConnection(url, username, password)
+    } catch (se: SQLException) {
+        se.printStackTrace()
+        return
+    }
+    val statement = con.createStatement()
+    val resultSet = statement.executeQuery("select * from CLIPBOARD_SUGGEST; ")
+    println(resultSet.toString())
 //    Spider.create(HeroListPageProcessor())
 //            .addUrl("https://dota2-zh.gamepedia.com/%E8%8B%B1%E9%9B%84")
 //            .addPipeline(ConsolePipeline())
